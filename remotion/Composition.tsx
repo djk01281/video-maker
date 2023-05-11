4e2270a4ff042ef843601eff3f3712e783b67e2a
djk01281 <djk01281@gmail.com>
2023-05-11 16:30:57 +0000

    chore: format files

--- remotion/Composition.tsx
+++ remotion/Composition.tsx
@@ -1,47 +1,59 @@
-import { AbsoluteFill, Sequence, useVideoConfig, Audio, staticFile, interpolate, useCurrentFrame } from "remotion";
+import {
+  AbsoluteFill,
+  Sequence,
+  useVideoConfig,
+  Audio,
+  staticFile,
+  interpolate,
+  useCurrentFrame,
+} from "remotion";
 import React from "react";
 import { Title } from "./Title";
 import { Description } from "./Description";
 
 export const MyComp = () => {
-    const { fps, durationInFrames, width, height } = useVideoConfig();
-    const titlesJson = require('../public/titles.json');
-    const urlsJson = require('../public/urls.json');
-    const titles: string[] = titlesJson.titles;
-    const urls: string[] = urlsJson.urls;
+  const { fps, durationInFrames, width, height } = useVideoConfig();
+  const titlesJson = require("../public/titles.json");
+  const urlsJson = require("../public/urls.json");
+  const titles: string[] = titlesJson.titles;
+  const urls: string[] = urlsJson.urls;
 
-    const descriptionElements: React.Component[] = [];
-    for (let i = 0; i < 5; i++) {
-        descriptionElements.push(
-            <Sequence durationInFrames={60} from={i * 60} style={{
-            }}>
-                <Description place={i + 1} name={titles[i]} img={urls[i]}></Description>
-            </Sequence>)
-    }
-
-    // const frame = useCurrentFrame();
-    // const titleTopMargin = interpolate(frame, [0, 30], [0, 100], { extrapolateRight: "clamp" });
-  
-    return (
-        <AbsoluteFill
-            style={{
-                backgroundColor: "black",
-                zIndex: -2
-            }}
-        >
-            <Audio src={staticFile("/Craven Weight.mp3")} />
-            <Sequence durationInFrames={Infinity} from={0} style={{
-                position: 'absolute',
-                left: '0',
-                right: '0',
-                margin: `200px auto`,
-                height: 'fit-content',
-                width: 'fit-content',
-                zIndex: 3
-            }}>
-                <Title />
-            </Sequence>
-            {descriptionElements}
-        </AbsoluteFill>
+  const descriptionElements: React.Component[] = [];
+  for (let i = 0; i < 5; i++) {
+    descriptionElements.push(
+      <Sequence durationInFrames={60} from={i * 60} style={{}}>
+        <Description place={i + 1} name={titles[i]} img={urls[i]}></Description>
+      </Sequence>
     );
+  }
+
+  // const frame = useCurrentFrame();
+  // const titleTopMargin = interpolate(frame, [0, 30], [0, 100], { extrapolateRight: "clamp" });
+
+  return (
+    <AbsoluteFill
+      style={{
+        backgroundColor: "black",
+        zIndex: -2,
+      }}
+    >
+      <Audio src={staticFile("/Craven Weight.mp3")} />
+      <Sequence
+        durationInFrames={Infinity}
+        from={0}
+        style={{
+          position: "absolute",
+          left: "0",
+          right: "0",
+          margin: `200px auto`,
+          height: "fit-content",
+          width: "fit-content",
+          zIndex: 3,
+        }}
+      >
+        <Title />
+      </Sequence>
+      {descriptionElements}
+    </AbsoluteFill>
+  );
 };
--- remotion/Title.tsx
+++ remotion/Title.tsx
@@ -1,4 +1,10 @@
-import { AbsoluteFill, useVideoConfig, useCurrentFrame, interpolate, spring } from "remotion";
+import {
+  AbsoluteFill,
+  useVideoConfig,
+  useCurrentFrame,
+  interpolate,
+  spring,
+} from "remotion";
 import React from "react";
 import "./font.css";
 
@@ -8,31 +14,56 @@ export const Title = () => {
 
   const driver = spring({
     frame,
-    fps
+    fps,
   });
-  const titleBottmMargin = interpolate(driver, [0, 30], [0, 50], { extrapolateRight: "clamp" });
-  const degrees = [2.5, 5, 2.5, 0, -2.5, -5, -2.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const titleBottmMargin = interpolate(driver, [0, 30], [0, 50], {
+    extrapolateRight: "clamp",
+  });
+  const degrees = [
+    2.5, 5, 2.5, 0, -2.5, -5, -2.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ];
   const titleDegree = interpolate(frame, [0, 300], [0, 300]);
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    }}>
-      <div style={{ marginBottom: `50px`, transform: `rotateZ(${degrees[titleDegree % degrees.length]}deg)` }}>
-        <span style={
-          {
-            backgroundColor: '#4440D7', padding: '20px', borderRadius: '20px', color: '#FEE00C', fontSize: '100px', fontWeight: '800', fontFamily: 'Sora',
-          }
-        }>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          marginBottom: `50px`,
+          transform: `rotateZ(${degrees[titleDegree % degrees.length]}deg)`,
+        }}
+      >
+        <span
+          style={{
+            backgroundColor: "#4440D7",
+            padding: "20px",
+            borderRadius: "20px",
+            color: "#FEE00C",
+            fontSize: "100px",
+            fontWeight: "800",
+            fontFamily: "Sora",
+          }}
+        >
           TOP 5
         </span>
       </div>
       <div>
-        <span style={
-          { backgroundColor: '#FADD75', padding: '20px', borderRadius: '20px', color: 'black', fontSize: '40px', fontWeight: '800', fontFamily: 'Sora' }
-        }>
+        <span
+          style={{
+            backgroundColor: "#FADD75",
+            padding: "20px",
+            borderRadius: "20px",
+            color: "black",
+            fontSize: "40px",
+            fontWeight: "800",
+            fontFamily: "Sora",
+          }}
+        >
           MOST WATCHED NETFLIX SHOWS IN KOREA
         </span>
       </div>
